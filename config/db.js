eabda007edbf9507cbac3e3e45be50ac41aa7596
Ali-Shetia1 <alishetia98@gmail.com>
2025-11-18 08:09:58 +0200

    edited a typo in db.js

--- config/db.js
+++ config/db.js
@@ -10,7 +10,7 @@ async function connectDB() {
     if (cached.conn) {
         return cached.conn
     }
-    if (!cached.promis) {
+    if (!cached.promise) {
         const opts = {
             bufferCommands: false
         }
